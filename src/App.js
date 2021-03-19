1ffed77b902863abc64e987ce0793f6e85ce224d
OG-Jons <jonas@marschall.pro>
2021-03-19 16:20:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Alexia is gay</h1>
+      <Navigation />
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  );
+}
+
+export default App;
